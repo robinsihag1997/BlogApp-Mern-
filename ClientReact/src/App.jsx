f2ff4c6b544f9ec96bd000aacfb5600c7271eda6
Robin Sihag <114049396+robinsihag1997@users.noreply.github.com>
2024-02-05 15:17:51 +0530

    initalClient frontRoutes done

--- ClientReact/src/App.jsx
+++ ClientReact/src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Dashboard from "./Pages/Dashboard";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import NotFound from "./Pages/NotFound";
+import Projects from "./Pages/Projects";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-400">hello world</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/SignIn" element={<Signin />} />
+        <Route path="/SignUp" element={<Signup />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
