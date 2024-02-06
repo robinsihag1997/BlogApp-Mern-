b22a904bd8dd491cd16ecaa7e86c4a9778fb31d8
Robin Sihag <114049396+robinsihag1997@users.noreply.github.com>
2024-02-06 21:11:49 +0530

    (backend)sign in api done

--- Nodejs backend/controller/auth.controller.js
+++ Nodejs backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userModel = require("../model/user.model");
-
+require("dotenv").config();
 const signup = async (req, res) => {
   console.log(req.body);
   const { username, email, password } = req.body;
@@ -42,6 +43,55 @@ const signup = async (req, res) => {
   }
 };
 
+const signin = async (req, res) => {
+  console.log("route hit");
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
+
+  try {
+    const validUser = await userModel.findOne({ email });
+    if (!validUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      data: "internal server error",
+      Message: `${error.message}`,
+    });
+  }
+};
+
 module.exports = {
   signup,
+  signin,
 };
